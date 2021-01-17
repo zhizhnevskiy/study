540229bb49e185418cbbf18645f6bd2537afabfb
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2021-01-17 15:41:55 +0300

    study

---
+++ PHPStart/06/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "front controller";
+
+// echo "HI YURA<br>";
+// echo "your reguest: " . $_SERVER['REQUEST_URI'] . "<br>";
+
+// $string1 = "Он закончил цколу в 2000 году. Стал студентом в 2002.";
+// $pattern1 = "/200[0-9]/";
+// $result1 = preg_match($pattern1, $string1);
+// var_dump($result1);
+
+
+// $string2 = "Apples and oranges";
+// $pattern2 = "/p{2}/";
+// $result2 = preg_match($pattern2, $string2);
+// var_dump($result2);
